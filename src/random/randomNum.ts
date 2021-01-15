9c871ae41f97dda5449e314b9d2b36a9b493eccf
zhangchengliang <1274828542@qq.com>
2021-01-15 11:43:14 +0800

    feat: ts与vue结合

--- src/random/randomNum.ts
+++ src/random/randomNum.ts
@@ -1,10 +1,10 @@
-// /**
-//  *
-//  * @desc 生成指定范围[min, max]的随机数
-//  * @param  {Number} min
-//  * @param  {Number} max
-//  * @return {Number}
-//  */
+/**
+ *
+ * @desc 生成指定范围[min, max]的随机数
+ * @param  {Number} min
+ * @param  {Number} max
+ * @return {Number}
+ */
 function randomNum (min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
