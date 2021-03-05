b42484a9a8d370bb2962c124edc7a3a618b44e33
zhangchengliang <1274828542@qq.com>
2021-03-05 22:43:18 +0800

    fix: 卡住了。怎么正确输入声明文件

--- dist/browser/removeCookie.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * ```
- * utilsLibrary.removeCookie('zcl')
- * ```
- * @desc 根据name删除cookie
- * @param  {String} name
- */
-declare function removeCookie(name: string): void;
-export default removeCookie;
--- dist/function/throttle.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @description 节流函数
- * 当持续触发事件时，每隔一段时间，只执行一次事件
- * 适用于`resize`和`scroll`等函数的调用频率
- */
-declare function throttle(fn: (...args: any[]) => void, wait: number): () => void;
-export default throttle;
