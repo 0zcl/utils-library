44c9e936d6cebcc4a2929997ddca0f17627cb206
zhangchengliang <1274828542@qq.com>
2021-03-22 20:51:17 +0800

    feat: 暂存

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-interface Cache {
-    [index: string]: any;
-}
-declare const cache: Cache;
-export default cache;
--- dist/random/randomColor.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * ```
- * utilsLibrary.randomColor() // #1267de
- * ```
- * @desc 随机生成颜色
- * @return {String}
- */
-declare function randomColor(): string;
-export default randomColor;
